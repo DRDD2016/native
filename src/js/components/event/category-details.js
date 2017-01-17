140f9bdf1518c6e557876b75616e30105d9ce6b0
Anita <a.nita@hotmail.co.uk>
2017-01-17 00:17:51 +0000

    refactor: categoryDetails component

--- src/js/components/event/category-details.js
+++ src/js/components/event/category-details.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Button from '../common/Button';
 // import BarChart from '../../components/event/bar-chart';
 import colours from '../../../styles/colours';
 // import styles from '../../../styles';
@@ -9,9 +8,6 @@ import colours from '../../../styles/colours';
 const CategoryDetails = ({ category, data }) => {
 
   const categoryTitle = `W${category.substring(1)}`;
-  console.log('CATEGORY TITLE', categoryTitle);
-  // const tally = 4;
-  console.log('Data', data);
 
   const icons = {
     what: 'star',
@@ -19,78 +15,78 @@ const CategoryDetails = ({ category, data }) => {
     when: 'calendar'
   };
 
-  const styles = {
-    title: {
-      flex: 1,
-      fontSize: 12,
-      fontWeight: 'bold',
-      color: colours[category],
-      paddingTop: 10
-    },
-    left: {
-      // flex: 1.2,
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start'
-    },
-    centre: {
-      // flex: 3.8,
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      marginRight: 0
-    },
-    right: {
-
-    },
-    row: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center'
-    },
-    button: {
-      deselected: {
-        component: {
-          flex: 1,
-          backgroundColor: colours.white,
-          borderColor: colours[category],
-          borderWidth: 1,
-          borderRadius: 100,
-          paddingTop: 8,
-          paddingBottom: 8,
-          paddingLeft: 15,
-          paddingRight: 15,
-          marginBottom: 2
-        },
-        text: {
-          flex: 1,
-          fontSize: 12,
-          color: colours[category]
-        }
-      },
-      selected: {
-        component: {
-          flex: 1,
-          backgroundColor: colours[category],
-          borderColor: colours[category],
-          borderWidth: 1,
-          borderRadius: 100,
-          paddingTop: 8,
-          paddingBottom: 8,
-          paddingLeft: 15,
-          paddingRight: 15,
-          marginBottom: 2
-        }
-      },
-      text: {
-        flex: 1,
-        fontSize: 12,
-        color: colours.white,
-        justifyContent: 'space-around'
-      }
-    }
-  };
+  // const styles = {
+  //   title: {
+  //     flex: 1,
+  //     fontSize: 12,
+  //     fontWeight: 'bold',
+  //     color: colours[category],
+  //     paddingTop: 10
+  //   },
+  //   left: {
+  //     // flex: 1.2,
+  //     flexDirection: 'column',
+  //     justifyContent: 'flex-start',
+  //     alignItems: 'flex-start'
+  //   },
+  //   centre: {
+  //     // flex: 3.8,
+  //     flexDirection: 'column',
+  //     justifyContent: 'flex-start',
+  //     alignItems: 'flex-start',
+  //     marginRight: 0
+  //   },
+  //   right: {
+  //
+  //   },
+  //   row: {
+  //     flex: 1,
+  //     flexDirection: 'row',
+  //     justifyContent: 'space-between',
+  //     alignItems: 'center'
+  //   },
+  //   button: {
+  //     deselected: {
+  //       component: {
+  //         flex: 1,
+  //         backgroundColor: colours.white,
+  //         borderColor: colours[category],
+  //         borderWidth: 1,
+  //         borderRadius: 100,
+  //         paddingTop: 8,
+  //         paddingBottom: 8,
+  //         paddingLeft: 15,
+  //         paddingRight: 15,
+  //         marginBottom: 2
+  //       },
+  //       text: {
+  //         flex: 1,
+  //         fontSize: 12,
+  //         color: colours[category]
+  //       }
+  //     },
+  //     selected: {
+  //       component: {
+  //         flex: 1,
+  //         backgroundColor: colours[category],
+  //         borderColor: colours[category],
+  //         borderWidth: 1,
+  //         borderRadius: 100,
+  //         paddingTop: 8,
+  //         paddingBottom: 8,
+  //         paddingLeft: 15,
+  //         paddingRight: 15,
+  //         marginBottom: 2
+  //       }
+  //     },
+  //     text: {
+  //       flex: 1,
+  //       fontSize: 12,
+  //       color: colours.white,
+  //       justifyContent: 'space-around'
+  //     }
+  //   }
+  // };
 
   return (
     <View style={ { paddingTop: 80 } }>
@@ -99,23 +95,18 @@ const CategoryDetails = ({ category, data }) => {
 
           if (category === 'when') {
             return (
-              <View key={JSON.stringify(datum)}>
-                {
-                  i === 0 &&
-                  <View style={styles.left}>
-                    <Text style={styles.title}>{ categoryTitle }</Text>
-                  </View>
-                }
-                <View style={styles.centre}>
-                  <View style={styles.row}>
-                    <Button
-                      buttonStyle={styles.button.selected.component}
-                      textStyle={styles.button.selected.text}
-                    >
-                      <Icon name={icons[category]} size={16} color={colours[category]} />
+              <View key={JSON.stringify(datum)} style={{ flexDirection: 'row' }}>
+
+                <View style={{ width: 150 }}>
+                  <Text style={{ alignSelf: 'center' }}>{ i === 0 && categoryTitle }</Text>
+                </View>
+
+                <View>
+                  <View>
+                    <Icon.Button name={icons[category]} size={16} color={colours[category]} backgroundColor="#efefef" onPress={() => {}}>
                       { datum.date }
                       { datum.time || 'TBC' }
-                    </Button>
+                    </Icon.Button>
                   </View>
                 </View>
               </View>
