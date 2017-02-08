2cd116d74c232c2368d777e73ee23505268ab9ee
Anita <a.nita@hotmail.co.uk>
2017-02-08 16:55:33 +0000

    feat: editEvent actions #136

--- src/js/actions/event/data.js
+++ src/js/actions/event/data.js
@@ -8,6 +8,9 @@ export const GET_EVENT_FAILURE = 'GET_EVENT_FAILURE';
 export const PATCH_EVENT_REQUEST = 'PATCH_EVENT_REQUEST';
 export const PATCH_EVENT_SUCCESS = 'PATCH_EVENT_SUCCESS';
 export const PATCH_EVENT_FAILURE = 'PATCH_EVENT_FAILURE';
+export const EDIT_EVENT_REQUEST = 'EDIT_EVENT_REQUEST';
+export const EDIT_EVENT_SUCCESS = 'EDIT_EVENT_SUCCESS';
+export const EDIT_EVENT_FAILURE = 'EDIT_EVENT_FAILURE';
 
 export const getEventRequest = () => ({
   type: GET_EVENT_REQUEST
@@ -20,6 +23,20 @@ export const getEventSuccess = data => ({
 
 export const getEventFailure = error => ({
   type: GET_EVENT_FAILURE,
+  error
+});
+
+export const editEventRequest = () => ({
+  type: EDIT_EVENT_REQUEST
+});
+
+export const editEventSuccess = data => ({
+  type: EDIT_EVENT_SUCCESS,
+  data
+});
+
+export const editEventFailure = error => ({
+  type: EDIT_EVENT_FAILURE,
   error
 });
 
@@ -43,6 +60,29 @@ export function getEvent (token, event_id) {
       .catch(err => dispatch(getEventFailure(err)));
     })
     .catch(err => dispatch(getEventFailure(err)));
+  };
+}
+
+export function editEvent (token, event, event_id) {
+  return (dispatch) => {
+    dispatch(editEventRequest());
+    fetch(`http://localhost:3000/events/${event_id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        authorization: token
+      }
+    })
+    .then((res) => {
+      res.json()
+      .then((data) => {
+        console.log(data);
+        dispatch(editEventSuccess(data));
+      })
+      .catch(err => dispatch(editEventFailure(err)));
+    })
+    .catch(err => dispatch(editEventFailure(err)));
   };
 }
 
