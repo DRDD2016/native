d951e6377c4a87bf79627af7225fa57ec349d646
Anita <a.nita@hotmail.co.uk>
2017-02-10 15:08:39 +0000

    refactor: sockets channel replaces user_id to socketID to receive feedItem from server #110 plus simplifies feed reducer #6

--- src/js/reducers/feed.old.js
+++ src/js/reducers/feed.old.js
@@ -152,13 +152,24 @@ export default function feed (state = initialState, action) {
   switch (action.type) {
 
     case actions.GET_FEED_REQUEST:
-      return handleGetFeedRequest(state, action);
+      return {
+        ...state,
+        isFetching: true
+      };
 
     case actions.GET_FEED_SUCCESS:
-      return handleGetFeedSuccess(state, action);
+      return {
+        ...state,
+        isFetching: false,
+        data: state.data.concat(action.data)
+      };
 
     case actions.GET_FEED_FAILURE:
-      return handleGetFeedFailure(state, action);
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      };
 
     case actions.APPLY_FILTER:
     case actions.CLEAR_FILTER:
@@ -168,32 +179,6 @@ export default function feed (state = initialState, action) {
       return state;
   }
 }
-
-function handleGetFeedRequest (state, action) {
-
-  const newState = update(state, {
-    isFetching: { $set: action.isFetching }
-  });
-  return newState;
-}
-
-function handleGetFeedSuccess (state, action) {
-  const newState = update(state, {
-    isFetching: { $set: action.isFetching },
-    data: { $set: state.data.concat(action.data) }
-  });
-  return newState;
-}
-
-function handleGetFeedFailure (state, action) {
-
-  const newState = update(state, {
-    isFetching: { $set: action.isFetching },
-    error: { $set: action.error }
-  });
-  return newState;
-}
-
 
 function handleFilter (state, action) {
 
--- src/js/socket-router.js
+++ src/js/socket-router.js
@@ -4,15 +4,15 @@ import { getFeedRequest, getFeedSuccess, getFeedFailure } from './actions/feed';
 
 const location = 'http://localhost:3000'; // will be something different when server is hosted!
 const socket = io(`${location}/feed`, { transports: ['websocket'] });
-const token = 'FAKE TOKEN';
 const INIT_FEED = 'INIT_FEED';
 
-socket.on('connected', () => {
-  console.info('Server connection initialised.');
-  socket.emit(INIT_FEED, token);
-  store.dispatch(getFeedRequest());
-  // need access to user's id or token
-  socket.on(`feed: ${token}`, (data) => {
+socket.on('connected', (socketId) => {
+  // clients emits to the server (client emits INIT_FEED )
+  socket.emit(INIT_FEED);
+  store.dispatch(getFeedRequest()); // spinner
+
+  // server emits the 'uniqueID' - data- array of feed items
+  socket.on(socketId, (data) => {
     store.dispatch(getFeedSuccess(data));
   });
 
