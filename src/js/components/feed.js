937cff34f0c94eddbf4df08d2e323205e55c78eb
Anita <a.nita@hotmail.co.uk>
2017-02-11 13:53:49 +0000

    feat: adds Spinner to Feed component #6

--- src/js/components/feed.js
+++ src/js/components/feed.js
@@ -18,6 +18,9 @@ export default class Feed extends Component {
 
   render () {
     const { allEvents, feed, isFetching, handleSelection, displaySome, displayAll, feedIsFiltered, isShowHosting } = this.props;
+    if (isFetching) {
+      return <Spinner />;
+    }
     const mappedFeed = [].concat(feed).reverse().map((data, i) => {
       return (
         <FeedItem
