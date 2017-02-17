e07e3cb8c2c774bb6a8d6e6701951e426554fa51
Anita <a.nita@hotmail.co.uk>
2017-02-17 19:42:03 +0000

    refactor: adds spinner to confirm email #151

--- src/js/components/auth/confirm-email.js
+++ src/js/components/auth/confirm-email.js
@@ -5,6 +5,7 @@ import hoistNonReactStatic from 'hoist-non-react-statics';
 import { emailValidator as validate } from './form-validation';
 import { FormTextInput } from './form-components';
 import Button from '../common/Button';
+import Spinner from '../common/Spinner';
 import styles from '../../../styles';
 import colours from '../../../styles/colours';
 
@@ -48,8 +49,25 @@ class ConfirmEmail extends Component {
     }
   }
 
+  renderButton = () => {
+    const { handleSubmit, handleSubmitForm, isConfirming } = this.props;
+    console.log('isConfirming', isConfirming);
+    if (isConfirming) {
+      return <Spinner size="large" />;
+    }
+    return (
+      <Button
+        buttonStyle={inlineStyle.buttonStyle}
+        textStyle={inlineStyle.textStyle}
+        onPress={handleSubmit(handleSubmitForm)}
+      >
+        <Text>SUBMIT</Text>
+      </Button>
+    );
+  };
+
   render () {
-    const { handleSubmit, handleSubmitForm } = this.props;
+    // const { handleSubmit, handleSubmitForm } = this.props;
     return (
       <View style={{ flex: 1 }}>
         <View style={{ alignItems: 'center', marginTop: 50, marginBottom: 70 }}>
@@ -63,13 +81,7 @@ class ConfirmEmail extends Component {
           </View>
           { this.renderServerError() }
           <View style={ styles.row }>
-            <Button
-              buttonStyle={inlineStyle.buttonStyle}
-              textStyle={inlineStyle.textStyle}
-              onPress={handleSubmit(handleSubmitForm)}
-            >
-              <Text>SUBMIT</Text>
-            </Button>
+            { this.renderButton() }
           </View>
         </View>
       </View>
