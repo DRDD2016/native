81b277f7ea5892b6769f33a012d847a053871d04
Mina <wilhelmina.jane@gmail.com>
2017-02-17 22:48:49 +0000

    feat: functions to write/read user state to/from AsyncStorage #153

--- src/js/init-store.js
+++ src/js/init-store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { createNavigationEnabledStore } from '@exponent/ex-navigation';
 import thunkMiddleware from 'redux-thunk';
 import devTools from 'remote-redux-devtools';
+import throttle from 'lodash/throttle';
 import rootReducer from './reducers/';
+import { saveState, loadState } from './lib/persist-state';
 
 const createStoreWithNavigation = createNavigationEnabledStore({
   createStore,
@@ -21,4 +23,14 @@ export function initStore (initialState) {
   );
 }
 
-export const store = initStore();
+// initialise store with previous app state
+const persistedState = loadState();
+export const store = initStore(persistedState);
+
+// save current app state to AsyncStorage every 2 secs
+store.subscribe(throttle(() => {
+  console.log('SAVING STATE!');
+  saveState({
+    user: store.getState().user
+  });
+}, 2000));
