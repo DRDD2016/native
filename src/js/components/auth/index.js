bc1c150b08c57d81678ec7a45813b1a01bdc9ea9
Anita <a.nita@hotmail.co.uk>
2017-02-21 13:23:44 +0000

    chore: back to stateless component #121 #156

--- src/js/components/auth/index.js
+++ src/js/components/auth/index.js
@@ -1,59 +1,48 @@
-import React, { Component } from 'react';
-import { Image, Text, View, AsyncStorage } from 'react-native';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
 import Button from '../common/Button';
 import Router from '../../router';
 import colours from '../../../styles/colours';
 
 const logo = require('../../../img/sparkLoginLogo.png');
 
-export default class Index extends Component {
+export default function Index ({ navigator }) {
 
-  componentWillMount () {
-    AsyncStorage.getItem('spark_token')
-    .then((token) => {
-      if (token) {
-        this.props.navigator.push(Router.getRoute('navbar'));
-      }
-    });
-  }
+  const goToLogin = () => {
+    navigator.push(Router.getRoute('login'));
+  };
 
-  goToLogin = () => {
-    this.props.navigator.push(Router.getRoute('login'));
-  }
+  const goToSignup = () => {
+    navigator.push(Router.getRoute('signup'));
+  };
 
-  goToSignup = () => {
-    this.props.navigator.push(Router.getRoute('signup'));
-  }
-
-  render () {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#fff' }}>
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 0.2 }}>
-          <Image style={{ height: 100, width: 300 }} source={ logo } />
-        </View>
-        <View style={{ flex: 0.3 }}>
-          <Text style={{ fontSize: 18, fontWeight: '600', color: '#a39b9b' }}>
-            The easy way to organise parties, events,
-            share pictures and memories with friends, family and groups.
-          </Text>
-        </View>
-        <View style={{ flexDirection: 'row', position: 'absolute', bottom: 0 }}>
-          <Button
-            buttonStyle={{ flex: 1, height: 50, backgroundColor: colours.blue, justifyContent: 'center' }}
-            textStyle={{ alignSelf: 'center', color: '#fff', fontWeight: '300', fontSize: 20 }}
-            onPress={ this.goToLogin }
-          >
-            LOG IN
-          </Button>
-          <Button
-            buttonStyle={{ flex: 1, height: 50, backgroundColor: colours.orange, justifyContent: 'center' }}
-            textStyle={{ alignSelf: 'center', color: '#fff', fontWeight: '300', fontSize: 20 }}
-            onPress={ this.goToSignup }
-          >
-            SIGN UP
-          </Button>
-        </View>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#fff' }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: 0.2 }}>
+        <Image style={{ height: 100, width: 300 }} source={ logo } />
       </View>
-    );
-  }
+      <View style={{ flex: 0.3 }}>
+        <Text style={{ fontSize: 18, fontWeight: '600', color: '#a39b9b' }}>
+          The easy way to organise parties, events,
+          share pictures and memories with friends, family and groups.
+        </Text>
+      </View>
+      <View style={{ flexDirection: 'row', position: 'absolute', bottom: 0 }}>
+        <Button
+          buttonStyle={{ flex: 1, height: 50, backgroundColor: colours.blue, justifyContent: 'center' }}
+          textStyle={{ alignSelf: 'center', color: '#fff', fontWeight: '300', fontSize: 20 }}
+          onPress={ goToLogin }
+        >
+          LOG IN
+        </Button>
+        <Button
+          buttonStyle={{ flex: 1, height: 50, backgroundColor: colours.orange, justifyContent: 'center' }}
+          textStyle={{ alignSelf: 'center', color: '#fff', fontWeight: '300', fontSize: 20 }}
+          onPress={ goToSignup }
+        >
+          SIGN UP
+        </Button>
+      </View>
+    </View>
+  );
 }
