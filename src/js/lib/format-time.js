4a0eab776acb03da3c3809ac25aba615b2778227
Mina <wilhelmina.jane@gmail.com>
2017-02-22 14:39:29 +0000

    bugfix: parses time HH:hh strings and ISO strings separately #127

--- src/js/lib/format-time.js
+++ src/js/lib/format-time.js
@@ -1,10 +1,14 @@
 import moment from 'moment';
 
-moment.locale('en-gb');
+// moment.locale('en-gb');
 
 export default function formatTime (time) {
   if (time === '') {
     return time;
   }
-  return moment(time, 'HH:mm').format('HH:mm');
+  if (/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(time)) {
+
+    return moment(time).format('HH:mm');
+  }
+  return moment(time.time, 'HH:mm').format('HH:mm');
 }
