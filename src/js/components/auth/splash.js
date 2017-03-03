ae45e4d277affa94a6173f931aae682a3ab9c1f3
Anita <a.nita@hotmail.co.uk>
2017-03-03 23:21:55 +0000

    feat: adds check for offline/online #232

--- src/js/components/auth/splash.js
+++ src/js/components/auth/splash.js
@@ -1,31 +1,65 @@
 import React, { Component } from 'react';
-import { Image, View, AsyncStorage } from 'react-native';
+import { Image, View, AsyncStorage, NetInfo, Text } from 'react-native';
 import Router from '../../router';
 import initSocket from '../../socket-router';
 
 const logo = require('../../../img/sparkLoginLogo.png');
 
 export default class Splash extends Component {
+  constructor () {
+    super();
+    this.state = {
+      isConnected: null
+    };
+  }
 
   componentWillMount () {
     setTimeout(() => {
-      AsyncStorage.getItem('spark_token')
-      .then((token) => {
-        if (token) {
-          initSocket();
-          this.props.navigator.push(Router.getRoute('navbar'));
-        } else {
-          this.props.navigator.push(Router.getRoute('auth'));
-        }
-      });
+      if (this.state.isConnected) {
+        AsyncStorage.getItem('spark_token')
+        .then((token) => {
+          if (token) {
+            initSocket();
+            this.props.navigator.push(Router.getRoute('navbar'));
+          } else {
+            this.props.navigator.push(Router.getRoute('auth'));
+          }
+        });
+      } else {
+        this.props.navigator.showLocalAlert('You are not connected to Internet!', {
+          text: { color: '#fff' },
+          container: { backgroundColor: 'red' }
+        });
+      }
     }, 2000);
   }
+
+  componentDidMount () {
+    NetInfo.isConnected.addEventListener(
+      'change',
+      this._handleConnectivityChange
+    );
+  }
+
+  componentWillUnmount () {
+    NetInfo.isConnected.removeEventListener(
+        'change',
+        this._handleConnectivityChange
+    );
+  }
+
+  _handleConnectivityChange = (isConnected) => {
+    this.setState({
+      isConnected
+    });
+  };
 
   render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#fff' }}>
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 0.2 }}>
           <Image style={{ height: 100, width: 300 }} source={ logo } />
+          <Text>{this.state.isConnected ? 'Online' : 'Offline'}</Text>
         </View>
       </View>
     );
