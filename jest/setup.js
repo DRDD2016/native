0e793e92592c654fc51398eb146a83aaa5cf1535
Anita <a.nita@hotmail.co.uk>
2017-03-04 16:54:41 +0000

    chore: set up mocks for NetInfo

--- jest/setup.js
+++ jest/setup.js
@@ -1,13 +1,3 @@
-jest.mock('Linking', () => {
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: jest.fn()
-  };
-});
-
 jest.mock('NetInfo', () => {
   return {
     isConnected: {
@@ -15,7 +5,8 @@ jest.mock('NetInfo', () => {
         return new Promise((accept, resolve) => { //eslint-disable-line
           accept(true);
         });
-      }
+      },
+      addEventListener: jest.fn()
     }
   };
 });
