047b1f532a822d3a970ca26885677bcba89d33ce
Anita <a.nita@hotmail.co.uk>
2017-03-04 15:49:34 +0000

    chore: atempt to solve duplication error

---
+++ jest/setup.js
@@ -0,0 +1,21 @@
+jest.mock('Linking', () => {
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    openURL: jest.fn(),
+    canOpenURL: jest.fn(),
+    getInitialURL: jest.fn()
+  };
+});
+
+jest.mock('NetInfo', () => {
+  return {
+    isConnected: {
+      fetch: () => {
+        return new Promise((accept, resolve) => { //eslint-disable-line
+          accept(true);
+        });
+      }
+    }
+  };
+});
