3c57b9ad2dba0b816cfed983c6c5347ddb7e383e
Anita <a.nita@hotmail.co.uk>
2017-03-04 16:25:13 +0000

    chore: another solution

---
+++ __mocks__/react-native.js
@@ -0,0 +1,12 @@
+const rn = require('react-native');
+
+jest.mock('Linking', () => {
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    openURL: jest.fn(),
+    canOpenURL: jest.fn(),
+    getInitialURL: jest.fn()
+  };
+});
+module.exports = rn;
--- jest/setup.js
+++ jest/setup.js
@@ -1,22 +1,22 @@
-global.Promise = require.requireActual('promise');
-jest.mock('Linking', () => {
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: jest.fn()
-  };
-});
-
-jest.mock('NetInfo', () => {
-  return {
-    isConnected: {
-      fetch: () => {
-        return new Promise((accept, resolve) => { //eslint-disable-line
-          accept(true);
-        });
-      }
-    }
-  };
-});
+// global.Promise = require.requireActual('promise');
+// jest.mock('Linking', () => {
+//   return {
+//     addEventListener: jest.fn(),
+//     removeEventListener: jest.fn(),
+//     openURL: jest.fn(),
+//     canOpenURL: jest.fn(),
+//     getInitialURL: jest.fn()
+//   };
+// });
+//
+// jest.mock('NetInfo', () => {
+//   return {
+//     isConnected: {
+//       fetch: () => {
+//         return new Promise((accept, resolve) => { //eslint-disable-line
+//           accept(true);
+//         });
+//       }
+//     }
+//   };
+// });
