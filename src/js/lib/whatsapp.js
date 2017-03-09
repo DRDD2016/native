a4e3f9bd201a70bda14da07759914662bdfa6627
Anita <a.nita@hotmail.co.uk>
2017-03-09 11:58:09 +0000

    fix: trims and spaces replacement to firstname, lastname and event name #291

--- src/js/lib/whatsapp.js
+++ src/js/lib/whatsapp.js
@@ -1,10 +1,17 @@
 import { Linking } from 'react-native';
 
+/**
+ * trimAndReplaceSpaces trim the text and replaces spaces with %20
+ * @param {string} text - event name or firstname or surname
+ * @returns {string}
+ */
+
+function trimAndReplaceSpaces (text) {
+  return text.trim().replace(/ /g, '%20');
+}
+
 export function composeWhatsAppMessage (user, event, code) {
-  const trimmed = event.name.trim();
-  const hasSpace = /\s/g.test(trimmed);
-  const eventName = hasSpace ? trimmed.replace(/ /g, '%20') : trimmed;
-  return `Hi%21%20${user.firstname}%20${user.surname}%20has%20invited%20you%20to%20the%20event%20%22${eventName}%22%20on%20Spark%2E%20Open%20or%20download%20the%20app%20and%20enter%20the%20following%20code%3A%20${code}`; // eslint-disable-line max-len
+  return `Hi%21%20${trimAndReplaceSpaces(user.firstname)}%20${trimAndReplaceSpaces(user.surname)}%20has%20invited%20you%20to%20the%20event%20%22${trimAndReplaceSpaces(event.name)}%22%20on%20Spark%2E%20Open%20or%20download%20the%20app%20and%20enter%20the%20following%20code%3A%20${code}`; // eslint-disable-line max-len
 }
 
 export function openWhatsApp (text) {
