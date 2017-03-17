03b47ab42da5a44db2828c8773e5acd7c3b1d0f8
Mina <wilhelmina.jane@gmail.com>
2017-03-17 13:25:04 +0000

    pops to top of create

--- src/js/lib/discard-event.js
+++ src/js/lib/discard-event.js
@@ -1,6 +1,9 @@
-import { store } from '../init-store';
+import { NavigationActions } from '@exponent/ex-navigation';
 import { clearCreateEvent } from '../actions/create';
+import { store } from '../init-store';
 
 export default function discardEvent () {
   store.dispatch(clearCreateEvent());
+  const navigatorUID = store.getState().navigation.currentNavigatorUID;
+  store.dispatch(NavigationActions.popToTop(navigatorUID));
 }
