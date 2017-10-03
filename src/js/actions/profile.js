ea9f11f2f94968df02ccee44570847ecb1aee4d3
rickardinho <d.rickardinho@gmail.com>
2017-10-03 21:08:09 +0100

    commitConsoleLogs

--- src/js/actions/profile.js
+++ src/js/actions/profile.js
@@ -74,6 +74,9 @@ export function editName (token, user_id, firstname, surname) {
 }
 
 export function uploadPhoto (token, formData) {
+  console.log('Photo formData: ', formData);
+  console.log('UploadPhotoURL: ', `${Config.URI}/upload`);
+  console.log('token: ', token);
   return (dispatch) => {
     dispatch(uploadPhotoRequest());
     fetch(`${Config.URI}/upload`, {
@@ -88,11 +91,16 @@ export function uploadPhoto (token, formData) {
     .then((res) => {
       res.json()
       .then((data) => {
+        console.log('Photo response data: ', data);
         dispatch(uploadPhotoSuccess(data));
       })
-      .catch(() => dispatch(uploadPhotoFailure('Something went wrong')));
+      .catch((e) => {
+        console.log('Photo response error: ', e);
+        dispatch(uploadPhotoFailure('Something went wrong'));
+      });
     })
-    .catch(() => {
+    .catch((e) => {
+      console.log('Photo response error: ', JSON.stringify(e));
       dispatch(uploadPhotoFailure('Something went wrong'));
     });
   };
