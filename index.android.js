9cab589e182f95993cb9dec185d9dff0e7194fa3
rickardinho <d.rickardinho@gmail.com>
2018-04-15 23:48:09 +0100

    commitClean

--- index.android.js
+++ index.android.js
@@ -1,8 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './src/js/index';
 
-console.ignoredYellowBox = [
-  'Setting a timer'
-];
 
 AppRegistry.registerComponent('Spark', () => App);
