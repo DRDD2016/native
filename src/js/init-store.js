932cf34323992725b4046ecf26a1c16a32b37725
rickardinho <d.rickardinho@gmail.com>
2018-04-18 22:44:38 +0100

    commitAddCalendarToPersist

--- src/js/init-store.js
+++ src/js/init-store.js
@@ -1,7 +1,7 @@
 import { AsyncStorage, Platform } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 // import { composeWithDevTools } from 'remote-redux-devtools';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import { createFilter } from 'redux-persist-transform-filter';
@@ -23,7 +23,7 @@ export function initStore (initialState) {
     rootReducer,
     initialState,
     compose(  // composeWithDevTools if debugging redux android
-      applyMiddleware(thunkMiddleware, logger), // remove logger from last in chain when not debugging
+      applyMiddleware(thunkMiddleware), // remove logger from last in chain when not debugging
       autoRehydrate({ log: true })
     )
   );
@@ -51,6 +51,13 @@ const saveSubsetFilter2 = createFilter(
   ]
 );
 
+const saveSubsetFilter3 = createFilter(
+  'calendar',
+  [
+    'data'
+  ]
+);
+
 // add another subset filter for calendar
 
 export const persistor = persistStore(
@@ -59,9 +66,10 @@ export const persistor = persistStore(
     storage: AsyncStorage,
     transforms: [
       saveSubsetFilter1,
-      saveSubsetFilter2
+      saveSubsetFilter2,
+      saveSubsetFilter3
     ],
-    whitelist: ['user', 'feed'],
+    whitelist: ['user', 'feed', 'calendar'],
     debounce: 2000
   }
 );
